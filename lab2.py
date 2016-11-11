d8acb01317526c79b4de60f082ad53b7f8cf56a4
Elvensong <trail.of.gods@gmail.com>
2016-11-11 20:58:59 +0700

    discretize function first commit

--- lab2.py
+++ lab2.py
@@ -88,8 +88,191 @@ def replace(data_dict, outfile, logfile):
 	return data_dup
 
 
-def discretize():
-	pass
+def discretize(data, outputfile, logfile):
+	# Store the result
+	discretized_attrs = []
+
+    # Get the input
+	method = raw_input("Chia gio theo chieu rong (nhap w) hay chieu sau (nhap d): ")
+	if method != 'w' and method != 'd':
+		print ("Nhap sai tham so")
+		return
+	bin_num = raw_input("Nhap so luong gio can chia: ")
+	try:
+		bin_num = int(bin_num)
+	except:
+		print "Nhap sai tham so"
+		return
+
+	# Get numeric attributes in sorted order
+	sorted_attrs = getSortedAttribute(data)
+	if method == 'w':
+		for attr in sorted_attrs:
+			discretized_attrs.append(getWidthBinning(attr, bin_num))
+	elif method == 'd':
+		for attr in sorted_attrs:
+			bin = getDepthBinning(attr, bin_num)
+			if bin == None:
+				return
+			discretized_attrs.append(bin)
+
+    # Write output file
+	writeOutputDiscretize(discretized_attrs, data, outputfile)
+
+	# Write log file
+	writeLogDiscretize(discretized_attrs, data, logfile)
+
+	# Return for other usage (optional)
+	return discretized_attrs
+
+
+def writeLogDiscretize(discretized_attrs, data, logfile):
+	try:
+		with open(logfile, 'wt') as file:
+			for i in range(len(discretized_attrs)):
+				file.write('#thuoc tinh: ')
+				file.write(data['meta'][i] + ' ')
+				for bin in discretized_attrs[i]:
+					if bin:
+						file.write(str(bin[0]) + '-' + str(bin[len(bin) - 1]) + ': ')
+						file.write(str(len(bin)) + ' ')
+
+				file.write('\n')
+	except:
+		print "Loi khi viet log file"
+		return
+
+
+def writeOutputDiscretize(discretized_attrs, data, outputfile):
+	try:
+		with open(outputfile, 'wt') as file:
+			file.write('sepal_length,sepal_width,petal_length,petal_width,class\n')
+			for row in data['data']:
+				bin_num = 0
+				for attr in row:
+					if not isinstance(attr, str):
+						# Convert to new data
+						new_data = findBin(discretized_attrs[bin_num], attr)
+						# Write to file
+						file.write(new_data + ',')
+					else:
+						file.write(attr)
+					bin_num += 1
+				file.write('\n')
+	except:
+		print "Loi khi viet outputfile"
+		return
+
+
+def findBin(discretized_attrs, attr):
+	# find attr in each bin
+	for bin in discretized_attrs:
+		if not bin:
+			continue
+		if attr > bin[len(bin) - 1]:
+			continue
+		else:
+			# find range of this bin
+			range = str(bin[0]) + '-' + str(bin[len(bin) - 1])
+			return range
+
+	# Cannot find the suitable bin put it in the second last bin
+	last_bin = discretized_attrs[len(discretized_attrs) - 2]
+	range = str(last_bin[0]) + '-' + str(last_bin[len(last_bin) - 1])
+	return range
+
+
+def getDepthBinning(attr, bin_num):
+	if bin_num > len(attr):
+		print "So luong bin lon hon so luong phan tu"
+		return None
+
+	bin_list = []
+	all_bins = []
+	index = 0
+
+    # if there are extra add it to the bin list until 0
+	extra = len(attr) % bin_num
+
+    # Number of intial bin's capacity
+	current_count = len(attr) / bin_num
+	for i in range(bin_num):
+		bin_list.append(current_count)
+	for i in range(extra):
+		bin_list[i] += 1
+
+    # Put to the bin
+	for bin_depth in bin_list:
+		bin = []
+		for i in range(bin_depth):
+			bin.append(attr[index + i])
+		all_bins.append(bin)
+		index += i + 1
+	return all_bins
+
+def getWidthBinning(attr, bin_num):
+	index = 0
+	all_bins = []
+
+    # find width for partition
+	width_partition = float(attr[len(attr) - 1] - attr[0]) / bin_num
+	lower_bound = attr[index]
+	for partition in range(bin_num):
+		flag = True
+		upper_bound = width_partition + lower_bound
+		while flag:
+			bin_list = []
+            # There is no data left for this bin
+			#if index == len(attr) - 1:
+				#all_bins.append(bin_list)
+				#break
+
+            # Put data to the bin
+			for i in range(index, len(attr)):
+				item = attr[i]
+				if attr[i] <= upper_bound+0.0001:
+					bin_list.append(attr[i])
+				else:
+					index = i
+					all_bins.append(bin_list)
+					flag = False
+					break
+
+            # The last data has been put to the bin
+			if flag == True:
+				index = i
+				all_bins.append(bin_list)
+				flag = False
+
+			lower_bound = upper_bound
+	return all_bins
+
+
+
+def getSortedAttribute(data):
+	attr1, attr2, attr3, attr4 = [], [], [], []
+	for datum in data['data']:
+		if datum[0] != '?':
+			attr1.append(datum[0])
+		else:
+			attr1.append(-1)
+		if datum[1] != '?':
+			attr2.append(datum[1])
+		else:
+			attr2.append(-1)
+		if datum[2] != '?':
+			attr3.append(datum[2])
+		else:
+			attr3.append(-1)
+		if datum[3] != '?':
+			attr4.append(datum[3])
+		else:
+			attr4.append(-1)
+
+	attr1.sort(), attr2.sort(), attr3.sort(), attr4.sort()
+	return (attr1, attr2, attr3, attr4)
+
+
 
 def normalize(data,logfile):
 	method = raw_input("Ban hay nhap phuong phap chuan hoa (min-max, z-score): ").strip()
@@ -151,11 +334,12 @@ def unshared_copy(inList):
     return inList
 
 if __name__ == "__main__":
-	data = read_file("small.dat")
+	data = read_file("data.dat")
 	outfile = "output.txt"
 	# pprint(data["data"])
 	# print find_most_frequent(data, -1)
 	# print find_num_missing(data, -1)
-	replace(data, "lol", "log.txt")
+	#replace(data, "lol", "log.txt")
 	# normalize(data,"log.txt")
 	# write_file(outfile,data)
+	discretize(data, outfile, 'logfile.txt')
