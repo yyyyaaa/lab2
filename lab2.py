9d980f2c9aa0b06c9f8c892132c0717c8da4fedf
yyyyaaa <yaphats280396@gmail.com>
2016-11-15 12:04:03 +0700

    Merge resolved

--- lab2.py
+++ lab2.py
@@ -233,23 +233,22 @@ def getSortedAttribute(data):
 	return (attr1, attr2, attr3, attr4)
 
 
-def min_max(minc,maxc,value,newmin=0.0,newmax=1.0):
-	return ((float(value)-minc)/(maxc-minc))*(newmax-newmin)+newmin
-def normalize(data,logfile):
+def normalize(data, outfile,logfile):
+	data_dup = unshared_copy(data)
 	method = raw_input("Ban hay nhap phuong phap chuan hoa (min-max, z-score): ").strip()
 	#method = "z-score"
 	log = open(logfile, 'wt')
 	if (method == "min-max"):
 		newmin = float(raw_input("Moi ban nhap gia tri min moi:"))
 		newmax = float(raw_input("Moi ban nhap gia tri max moi:"))
-		numcol = len(data["meta"])-1
-		m = [[min(col),max(col)] for col in [[x[i] for x in data["data"] if (x[i]!="?")] for i in range(numcol)]]
-		for index1,row in enumerate(data["data"]):
+		numcol = len(data_dup["meta"])-1
+		m = [[min(col),max(col)] for col in [[x[i] for x in data_dup["data"] if (x[i]!="?")] for i in range(numcol)]]
+		for index1,row in enumerate(data_dup["data"]):
 			for index2,col in enumerate(row):
 				if index2 < numcol and col != "?":
-					value = min_max(m[index2][0],m[index2][1],data["data"][index1][index2],newmin,newmax)
-					data["data"][index1][index2] = value
-					log.write("# thuoctinh: %s, %f\n" %(data["meta"][index2],value))
+					value = min_max(m[index2][0],m[index2][1],data_dup["data"][index1][index2],newmin,newmax)
+					data_dup["data"][index1][index2] = value
+					log.write("# thuoctinh: %s, %f\n" %(data_dup["meta"][index2],value))
 		#print data
 
 	if (method == "z-score"):
@@ -290,7 +289,8 @@ def find_num_missing(data_dict, field_index):
 	how_many = len([line[field_index] for line in data_dict['data'] if line[field_index] == "?"])
 	return how_many
 
-
+def min_max(minc,maxc,value,newmin=0.0,newmax=1.0):
+	return ((float(value)-minc)/(maxc-minc))*(newmax-newmin)+newmin
 
 def unshared_copy(inList):
     if isinstance(inList, list):
